05a8ea8fca2d8775a4946ad4578404b828904074
Legendword <zwh@legendword.com>
2023-07-28 21:12:52 -0700

    Test API endpoint

--- backend/src/main/java/com/cpsc304/anything/AnythingApplication.java
+++ backend/src/main/java/com/cpsc304/anything/AnythingApplication.java
@@ -2,9 +2,11 @@ package com.cpsc304.anything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cpsc304.anything.database.DatabaseConnectionHandler;
 
 @SpringBootApplication
 public class AnythingApplication {
+    private DatabaseConnectionHandler dbHandler = null;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnythingApplication.class, args);
