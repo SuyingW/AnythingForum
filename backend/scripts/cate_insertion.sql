041d95c5a6b359defe537fe4fd73ba68d1491680
Diana <dianawq17@gmail.com>
2023-08-02 14:59:30 -0700

    Posts and category script

---
+++ backend/scripts/cate_insertion.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO "Category" (categoryID, categoryName)
+VALUES (1, 'Education');
+
+INSERT INTO "Category" (categoryID, categoryName)
+VALUES (2, 'Animal';
+
+INSERT INTO "Category" (categoryID, categoryName)
+VALUES (3, 'University');
---
+++ backend/scripts/posts_insertion.sql
@@ -0,0 +1,20 @@
+INSERT INTO "Post" (postID, content, title, publishDate, categoryID, userID)
+VALUES (1, 'CPSC 304 is such a great course!!!', 'My thoughts after the midterm',
+        TO_DATE('2023-07-24 15:37:59', 'YYYY-MM-DD HH24:MI:SS'), 1, 1);
+
+INSERT INTO "Post" (postID, content, title, publishDate, categoryID, userID)
+VALUES (2, 'ChatGPT is not going to replace jobs, but it certainly will make
+teachers'' jobs much harder.', 'What about ChatGPT?', TO_DATE('2023-07-24 21:25:31',
+'YYYY-MM-DD HH24:MI:SS'), 1, 1);
+
+INSERT INTO "Post" (postID, content, title, publishDate, categoryID, userID)
+VALUES (3, 'Please do not post any offensive or inappropriate content, thanks!',
+           'Community Rules', TO_DATE('2023-07-24 21:34:13', 'YYYY-MM-DD HH24:MI:SS'), 6, 4);
+
+INSERT INTO "Post" (postID, content, title, publishDate, categoryID, userID)
+VALUES (4, 'Add me as a friend and I'' ll send you a surprise.',
+           'Add friend!', TO_DATE('2023-07-24 21:39:52', 'YYYY-MM-DD HH24:MI:SS'), 5, 5);
+
+INSERT INTO "Post" (postID, content, title, publishDate, categoryID, userID)
+VALUES (5, 'I really don''t know what to post.', 'Something', TO_DATE('2023-07-24
+    21:46:11', 'YYYY-MM-DD HH24:MI:SS'), 3, 1);
